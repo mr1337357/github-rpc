00f70fd1936135060a86f2ecb6dbdd45b1edaf67
Your Name <you@example.com>
2020-08-05 00:52:47 -0700

    me big dumb

--- github-rpc.py
+++ github-rpc.py
@@ -5,14 +5,14 @@ import sys
 import os
 import time
 
-if len(sys.argv)<2:
-    sys.stderr.write('need a port number\n')
+#if len(sys.argv)<2:
+#    sys.stderr.write('need a port number\n')
 
 #sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 #sock.bind(('0.0.0.0',int(sys.argv[1])))
 
 while True:
-    time.sleep('300')
+    time.sleep(300)
     try:
         os.system('git pull')
         os.system('bash runme.sh')
