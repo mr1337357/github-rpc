4d849ca6e584f6c82dd8cc9d48115b9a56ed28f2
Your Name <you@example.com>
2020-08-05 00:50:51 -0700

    first push

---
+++ github-rpc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import socket
+import sys
+import os
+import time
+
+if len(sys.argv)<2:
+    sys.stderr.write('need a port number\n')
+
+#sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+#sock.bind(('0.0.0.0',int(sys.argv[1])))
+
+while True:
+    time.sleep('300')
+    os.system('git pull')
+    os.system('bash runme.sh')
